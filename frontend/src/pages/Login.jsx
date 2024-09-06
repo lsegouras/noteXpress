caea57faa2ee59afbff5f1836c9d000e638d6b9d
Laura Segouras <lausegouras@gmail.com>
2024-09-06 14:03:23 -0300

    Login page and styles

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,18 @@
+import { Link } from 'react-router-dom'
+import Form from '../components/Form'
+import '../styles/Login.css'
+
 const Login = () => {
-  return <div>Login</div>
+  return (
+    <div className="login-container">
+      <div className="form-container">
+        <Form route="/api/token/" method="login" />
+        <p className="register-link">
+          Ainda não tem uma conta? <Link to="/register">Registre-se aqui</Link>
+        </p>
+      </div>
+    </div>
+  )
 }
 
 export default Login
