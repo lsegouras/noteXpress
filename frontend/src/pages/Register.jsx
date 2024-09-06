d834485df361adfc3d91ae02d0fd2de9b9cde361
Laura Segouras <lausegouras@gmail.com>
2024-09-06 14:03:06 -0300

    Register page and styles

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,14 @@
+import Form from '../components/Form'
+import '../styles/Register.css'
+
 const Register = () => {
-  return <div>Register</div>
+  return (
+    <div className="register-container">
+      <div className="form-container">
+        <Form route="/api/user/register/" method="register" />
+      </div>
+    </div>
+  )
 }
 
 export default Register
