545a564a87a63d767849f3e40c79656e583fed9f
Laura Segouras <lausegouras@gmail.com>
2024-10-21 14:50:19 -0300

    notes api serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Note
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,6 +9,16 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
+        # sourcery skip: inline-immediately-returned-variable
         print(validated_data)
         user = User.objects.create_user(**validated_data)
         return user
+
+class NoteSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Note
+    fields = ["id", "title", "content", "created_at", "author"]
+    extra_kwargs = {"author": {"read_only": True}}
+
+
+
